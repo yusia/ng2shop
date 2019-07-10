69fece5cf94d58a21a28ee77c837908253fd73f4
yulia antonova <yusiagerachka@gmail.com>
2019-07-10 23:18:51 +0300

    init friends shop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { products } from './products';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shop';
+  title = 'Friends shop';
+  products = products;
 }
